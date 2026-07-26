269dcc47a330338bdaed0a0eda89418172ec6844
CASPER <bmemesil@gmail.com>
2026-07-26 14:45:48 +0300

    Cari kart: saveCariData global'e tasindi (odeme al / cikis yaptir ReferenceError duzeltildi)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GümrükPortal Service Worker v5
-const CACHE_NAME = 'gumrukportal-v7';
+const CACHE_NAME = 'gumrukportal-v8';
 
 self.addEventListener('install', event => {
   console.log('[SW] v6 Yüklendi');
@@ -7,7 +7,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  console.log('[SW] v7 Aktif');
+  console.log('[SW] v8 Aktif');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
