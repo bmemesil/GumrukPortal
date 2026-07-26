b5573f3f3af6590dfb28ab7304640efa201f5704
CASPER <bmemesil@gmail.com>
2026-07-26 15:44:06 +0300

    Analiz Dashboard: detay tablosu secili donemi izliyor, tum panellerde gelen/cekilen ayrimi, cari uyarisi TL borcunu da sayiyor

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GümrükPortal Service Worker v5
-const CACHE_NAME = 'gumrukportal-v9';
+const CACHE_NAME = 'gumrukportal-v10';
 
 self.addEventListener('install', event => {
   console.log('[SW] v6 Yüklendi');
@@ -7,7 +7,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  console.log('[SW] v9 Aktif');
+  console.log('[SW] v10 Aktif');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
