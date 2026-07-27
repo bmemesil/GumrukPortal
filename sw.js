c19c9798ec4897eb5ab44bafd8d48810235f554b
CASPER <bmemesil@gmail.com>
2026-07-27 21:09:29 +0300

    Yonetim menusune Arac Kayit Listesi kisayolu (sw v13)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GümrükPortal Service Worker v5
-const CACHE_NAME = 'gumrukportal-v12';
+const CACHE_NAME = 'gumrukportal-v13';
 
 self.addEventListener('install', event => {
   console.log('[SW] v6 Yüklendi');
@@ -7,7 +7,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  console.log('[SW] v12 Aktif');
+  console.log('[SW] v13 Aktif');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
