f6967b775e5cf7f23215d12ccd7b307d60edfe1c
CASPER <bmemesil@gmail.com>
2026-08-09 17:51:10 +0300

    Sol menu fare tekerlegiyle kaydirilabilir (sw v24)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GümrükPortal Service Worker v5
-const CACHE_NAME = 'gumrukportal-v23';
+const CACHE_NAME = 'gumrukportal-v24';
 
 self.addEventListener('install', event => {
   console.log('[SW] v6 Yüklendi');
@@ -7,7 +7,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  console.log('[SW] v23 Aktif');
+  console.log('[SW] v24 Aktif');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
