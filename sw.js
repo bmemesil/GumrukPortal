4444322b1df2ea117ec48a9926da930cf57bbc86
CASPER <bmemesil@gmail.com>
2026-08-09 18:09:44 +0300

    Vergi hesap: Damga V. varsayilan 1.605,80 (sw v26)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GümrükPortal Service Worker v5
-const CACHE_NAME = 'gumrukportal-v25';
+const CACHE_NAME = 'gumrukportal-v26';
 
 self.addEventListener('install', event => {
   console.log('[SW] v6 Yüklendi');
@@ -7,7 +7,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  console.log('[SW] v25 Aktif');
+  console.log('[SW] v26 Aktif');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
